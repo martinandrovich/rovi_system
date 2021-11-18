6181d034cd5f25b30c123c34df152a1d5862d4bb
Martin <martinandrovich@gmail.com>
2021-11-18 15:12:18 +0100

    example of examples framework

--- rovi_system/tests/template/test_template.cpp
+++ rovi_system/tests/template/test_template.cpp
@@ -8,6 +8,21 @@ main(int argc, char **argv)
 	ros::init(argc, argv, "test_template");
 	ros::NodeHandle nh;
 
+	// deduce experiment directory from node name ('test_template' → 'template')
+	// must be configured properly; see 'tests/template/README.md'
+	auto dir_experiment = rovi_system::get_experiment_dir(); // get_experiment_dir("template")
+
+	// let's do an experiment
+	// create a time-stampted data directory (tests/template/data/XXXXXX/)
+	auto dir_data = rovi_system::make_experiment_data_dir();
+
+	// print
+	std::cout << "dir_experiment: " << dir_experiment << std::endl;
+	std::cout << "dir_data: " << dir_data << std::endl;
+
+	// write some data
+	;
+
 	// exit
 	return 0;
 }
