820df7eb7f2f60df4a29aeb0c74950734951f170
Martin Androvich <martinandrovich@gmail.com>
2021-11-18 16:04:56 +0100

    change to struct + add methods to MATLAB code

--- rovi_system/scripts/rovi_system.m
+++ rovi_system/scripts/rovi_system.m
@@ -1,26 +1,17 @@
-
 % PLEASE LOAD THIS USING:
 % run("path/to/rovi_system.m")
 
 disp("Resolving directories for ROVI system...")
 
-% define directories
-DIR_CURRENT    = erase(strrep(which(mfilename),"\","/"), "/" + mfilename + ".m");
-DIR_PKG_ROOT   = DIR_CURRENT + "/..";
-DIR_SCRIPTS    = DIR_PKG_ROOT + "/scripts";
-DIR_EXPORT_FIG = DIR_SCRIPTS + "/export_fig";
-DIR_TESTS      = DIR_PKG_ROOT + "/tests";
-
-% DIR_EXPORT_FIG = DIR_CURRENT + "/export_fig";
-% DIR_ROOT       = DIR_CURRENT + "/../../../..";
-% DIR_ASSETS     = DIR_ROOT + "/assets";
-% DIR_WS         = DIR_ROOT + "/ws";
-% DIR_DATA       = DIR_WS + "/src/rovi_system/data";
-% DIR_IMGS       = DIR_ASSETS + "/img";
+% define directories struct
+DIR.SCRIPTS     = erase(strrep(which(mfilename),"\","/"), "/" + mfilename + ".m");
+DIR.ROVI_SYSTEM = DIR.SCRIPTS + "/..";
+DIR.EXPORT_FIG  = DIR.SCRIPTS + "/export_fig";
+DIR.TESTS       = DIR.ROVI_SYSTEM + "/tests";
 
 % configure export_fig
 % https://github.com/altmany/export_fig
-addpath(DIR_EXPORT_FIG);
+addpath(DIR.EXPORT_FIG);
 
 % configure figure defaults
 set(groot, "DefaultFigureRenderer", "painters");
@@ -28,46 +19,48 @@ set(groot, "DefaultFigurePosition", [0 0 500 500]);
 set(groot, "DefaultFigureColor", [1 1 1]);
 set(groot, "DefaultAxesFontSize", 14); % !!!
 
-% colors
-COL_GRAY = [200 200 200]/255;
-COL_LIGHTGRAY = [220 220 220]/255;
-COL_ORANGE = [255 143 0]/255;
-COL_BLUE = [0 207 255]/255;
-COL_MAGENTA = [236 88 234]/255;
+% colors struct
+COLOR.GRAY = [200 200 200]/255;
+COLOR.LIGHTGRAY = [220 220 220]/255;
+COLOR.ORANGE = [255 143 0]/255;
+COLOR.BLUE = [0 207 255]/255;
+COLOR.MAGENTA = [236 88 234]/255;
 
 % default MATLAB colors
 % http://math.loyola.edu/~loberbro/matlab/html/colorsInMatlab.html
-MATLAB_COLORS = {
-	[0.0000, 0.4470, 0.7410] ;
-	[0.8500, 0.3250, 0.0980] ;
-	[0.9290, 0.6940, 0.1250] ;
-	[0.4940, 0.1840, 0.5560] ;
-	[0.4660, 0.6740, 0.1880] ;
-	[0.3010, 0.7450, 0.9330] ;
-	[0.6350, 0.0780, 0.1840] ;
-};
+% MATLAB_COLORS = {
+% 	[0.0000, 0.4470, 0.7410] ;
+% 	[0.8500, 0.3250, 0.0980] ;
+% 	[0.9290, 0.6940, 0.1250] ;
+% 	[0.4940, 0.1840, 0.5560] ;
+% 	[0.4660, 0.6740, 0.1880] ;
+% 	[0.3010, 0.7450, 0.9330] ;
+% 	[0.6350, 0.0780, 0.1840] ;
+% };
 
-% log info
-% disp("DIR_ROOT: " + what(DIR_ROOT).path)
-% display("DIR_DATA: " + what(DIR_DATA).path)
-% disp("DIR_IMGS: " + what(DIR_IMGS).path)
+% print resolved directories
+disp("DIR.ROVI_SYSTEM: " + what(DIR.ROVI_SYSTEM).path)
+disp("DIR.SCRIPTS: " + what(DIR.SCRIPTS).path)
+disp("DIR.TESTS: " + what(DIR.TESTS).path)
 
-% functions
+% export local functions
+get_experiment_dir = @get_experiment_dir_;
+get_experiment_data_dir = @get_experiment_data_dir_;
 
-function dir = get_experiment_dir(experiment_name)
-%get_experiment_dir - Description
-%
-% Syntax: dir = get_experiment_dir(experiment_name)
-%
-% Long description
-	
+% local functions
+
+function dir = get_experiment_dir_(experiment_name)
+	dir_tests = evalin("base","DIR.TESTS");
+	dir = dir_tests + "/" + experiment_name;
+	dir = what(dir).path;
 end
 
-function dir = get_experiment_data_dir(experiment_name, timestamp)
-	%get_experiment_dir - Description
-	%
-	% Syntax: dir = get_experiment_dir(experiment_name)
-	%
-	% Long description
-		
+function dir = get_experiment_data_dir_(experiment_name, timestamp)
+	arguments
+		experiment_name
+		timestamp = ""
+	end
+	dir_tests = evalin("base","DIR.TESTS");
+	dir = dir_tests + "/" + experiment_name + "/data/" + timestamp;
+	dir = what(dir).path;
 end
---
+++ rovi_system/tests/template/test_template.m
@@ -0,0 +1,5 @@
+close all; clear; clc;
+run("../../scripts/rovi_system.m");
+
+dir_expr = get_experiment_dir("template");
+dir_data = get_experiment_data_dir("template");
