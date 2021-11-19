b5668ee2d48ad7501dcffb98b4b367b5356f2227
Martin <martinandrovich@gmail.com>
2021-11-19 16:32:18 +0100

    add reachability test

--- rovi_system/tests/reachability/test_reachability.cpp
+++ rovi_system/tests/reachability/test_reachability.cpp
@@ -1,16 +1,96 @@
 #include <iostream>
+#include <array>
+#include <vector>
+#include <fstream>
 
 #include <ros/ros.h>
+#include <ros_utils/ros_utils.h>
 #include <rovi_system/rovi_system.h>
+#include <ur5_planner/moveit.h>
+#include <ur5_planner/reachability.h>
 
 int
 main(int argc, char **argv)
 {
+	using namespace rovi_system;
+	using namespace geometry_msgs;
+
+	// first run:
+	// roslaunch ur5_gazebo ur5.launch ee:=wsg50 gazebo:=false moveit:=true rviz:=true
+	// or use 'test_reachability.launch'
+
 	// init ROS node
 	ros::init(argc, argv, "test_reachability");
 	ros::NodeHandle nh;
-	
-	std::cout << "Hello from test_reachability!" << std::endl;
+
+	// parameters
+	const auto OBJ_NAME   = "bottle";
+	const auto OBJ_POSE   = make_pose({ 0.5, 1.0, TABLE.HEIGHT });
+	const auto VISUALIZE  = (argc > 1) ? std::string(argv[1]) == "true" : false;
+	const auto RESOLUTION = VISUALIZE ? 16 : 180;
+
+	// ------------------------------------------------------------------------------
+
+	// sleep a bit to let moveit startup
+	ros::Duration(2.0).sleep();
+
+	// init moveit
+	ur5::moveit::init(nh);
+	ur5::moveit::start_scene_publisher(100);
+
+	// setup planning scene
+	// add collision objects to planning scene
+	ur5::moveit::add_cobjs(
+	{
+		{ "table",  TABLE.POSE },
+		{ "bottle", make_pose({ 0.50, 1.00, 0.75 }) }
+	}, "rovi_models");
+
+	// let everything settle
+	ros::Duration(2.0).sleep();
+
+	// ------------------------------------------------------------------------------
+
+	// generate base poses (discretize table)
+	std::vector<Pose> base_poses;
+	for (double y = 0; y < TABLE.WIDTH; y += 0.1)
+		for (double x = 0.; x < TABLE.LENGTH; x += 0.1)
+			base_poses.push_back(make_pose({ x, y, TABLE.HEIGHT - 0.01 }));
+
+	ROS_INFO_STREAM("Generated " << base_poses.size() << " base poses for reachability test...");
+	ros::Duration(1.0).sleep();
+
+	// perform reachability test for each of the grasping orientations (top and side)
+	for (std::string grasp_orientation : { "top", "side" })
+	{
+		// make experiment directory (once)
+		// static auto dir_data = rovi_system::make_custom_data_dir("reachability", "/" + grasp_orientation);
+		static auto dir_data = rovi_system::make_timestamped_data_dir("reachability");
+
+		// define grasping transformation
+		auto T_grasp = (grasp_orientation == "top") ? GRASP_TOP_AT_TCP(0.2) : GRASP_SIDE_AT_TCP(0.1);
+
+		// // iterate different base poses
+		std::vector<ur5::moveit::ReachabilityData> results;
+		for (const auto& pose : base_poses)
+		{
+			auto result = ur5::moveit::reachability(pose, OBJ_POSE, T_grasp, RESOLUTION, VISUALIZE);
+			results.push_back(result);
+		}
+
+		// write results to file
+		auto filename = dir_data + "/" + grasp_orientation + ".csv";
+		std::ofstream fs(filename, std::ofstream::out);
+
+		fs << "x [m], y [m], plausible states" << std::endl;
+		for (const auto& result : results)
+			fs << result.pos_base[0] << ", " << result.pos_base[1] << ", " << result.plausible_states << "\n";
+		fs.close();
+		ROS_INFO_STREAM("Data was written to '" << filename);
+	}
+
+	// cleanup
+	ur5::moveit::terminate();
 
 	// exit
 	return 0;
---
+++ rovi_system/tests/reachability/test_reachability.m
@@ -0,0 +1,36 @@
+close all; clear; clc;
+run("../../scripts/rovi_system.m");
+
+% deduce directories
+GRASP_POS = "top"; % "top" or "side"
+DIR_DATA = get_experiment_data_dir("reachability", "20211119_162442")
+DIR_IMGS = get_img_dir("reachability")
+
+% load data
+data = readmatrix(DIR_DATA + "/" + GRASP_POS + ".csv");
+
+increment = 0.1;
+x = [min(data(:, 2)) max(data(:, 2))];
+y = [min(data(:, 1)) max(data(:, 1))];
+
+C = [];
+for i = 1:size(data)
+	row = data(i, 1) * (1/increment) + 1;
+	col = data(i, 2) * (1/increment) + 1;
+	C(row, col) = data(i, 3);
+end
+
+% plot
+figure()
+set(gcf, "Position", [0 0 500 250]);
+imagesc(x, y, C)
+colorbar
+set(gca,"XAxisLocation","top","YAxisLocation","left","ydir","reverse");
+axis image;
+xlabel("Table width (y)")
+ylabel("Table height (x)")
+ytickformat("%.1f");
+xtickformat("%.1f");
+
+% export
+export_fig(DIR_IMGS + "/reachability-" + GRASP_POS + ".pdf", "-painters")
