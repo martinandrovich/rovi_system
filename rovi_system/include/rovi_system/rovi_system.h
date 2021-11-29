582899a3742e05e42edf6974ba3a6fe9ec2d4ae3
Martin <martinandrovich@gmail.com>
2021-11-29 21:54:43 +0100

    add spawn_obstacles() + make_timestamped_data_dirs()

--- rovi_system/include/rovi_system/rovi_system.h
+++ rovi_system/include/rovi_system/rovi_system.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <initializer_list>
 #include <ros/ros.h>
 #include <ros_utils/geometry_msgs.h>
 
@@ -16,6 +17,11 @@ namespace rovi_system
 		double MASS               = 10.0; // [kg]
 		geometry_msgs::Pose POSE  = geometry_msgs::make_pose({ 0.4, 0.6, 0.64 });
 	} TABLE;
+	
+	// -- setup -------------------------------------------------------------------
+	
+	void
+	spawn_obstacles();
 
 	// -- experiments -------------------------------------------------------------
 
@@ -24,6 +30,9 @@ namespace rovi_system
 
 	std::string
 	make_timestamped_data_dir(const std::string& experiment_name = "", const std::string& dir = "");
+	
+	std::string
+	make_timestamped_data_dirs(const std::string& experiment_name, const std::initializer_list<std::string>& dirs);
 	
 	std::string
 	make_custom_data_dir(const std::string& experiment_name, const std::string& dir);
--- rovi_system/src/lib_rovi_system.cpp
+++ rovi_system/src/lib_rovi_system.cpp
@@ -1,7 +1,16 @@
 #include <rovi_system/rovi_system.h>
 #include <filesystem>
 #include <ros/package.h>
-#include <ros_utils/std.h>
+#include <ros_utils/ros_utils.h>
+
+void
+rovi_system::spawn_obstacles()
+{
+	gazebo::spawn_model("coffecan", "coffecan1", { 0.105778, 0.146090, 0.740000 }, { 0.000015, 0.0, 0.142340 });
+	gazebo::spawn_model("mug", "mug1", { 0.238709, 0.223757, 0.740674 }, { -0.012724, -0.016048, 1.954001 });
+	gazebo::spawn_model("mug", "mug2", { 0.127200, 0.792169, 0.740701 }, { 0.012603, 0.016653, 2.844309 });
+	gazebo::spawn_model("crate", "crate1", { 0.641959, 0.676307, 0.739943 }, { 0.003127, -0.003012, 0.030163 });
+}
 
 std::string
 rovi_system::get_experiment_dir(std::string experiment_name)
@@ -16,7 +25,7 @@ rovi_system::get_experiment_dir(std::string experiment_name)
 	// check if directory exists; return if all good
 	if (not std::filesystem::exists(dir))
 		throw std::invalid_argument("The experiment directory: '" + dir + "' does not exist in get_experiment_dir().");
-	
+
 	// create sub-directoires
 	std::filesystem::create_directories(dir + "/data");
 	std::filesystem::create_directories(dir + "/img");
@@ -27,7 +36,6 @@ rovi_system::get_experiment_dir(std::string experiment_name)
 std::string
 rovi_system::make_timestamped_data_dir(const std::string& experiment_name, const std::string& dir)
 {
-
 	if (not dir.empty() and dir[0] != '/')
 		throw std::invalid_argument("The provided directory '" + dir + "' must begin with '/' in make_timestamped_data_dir().");
 
@@ -44,14 +52,36 @@ rovi_system::make_timestamped_data_dir(const std::string& experiment_name, const
 }
 
 std::string
+rovi_system::make_timestamped_data_dirs(const std::string& experiment_name, const std::initializer_list<std::string>& dirs)
+{
+
+	// if (not dir.empty() and dir[0] != '/')
+	// 	throw std::invalid_argument("The provided directory '" + dir + "' must begin with '/' in make_timestamped_data_dir().");
+
+	auto dir_data = get_experiment_dir(experiment_name) + "/data/" + get_timestamp(); // time-stamped data directory
+	auto dir_img  = get_experiment_dir(experiment_name) + "/img"; // img dir (for all experiments)
+
+	for (auto& dir : dirs)
+	{
+		if (std::filesystem::create_directories(dir_data + "/" + dir))
+			ROS_INFO_STREAM("Created time-stamped data directory: " << dir_data);
+	}
+
+	if (std::filesystem::create_directories(dir_img))
+		ROS_INFO_STREAM("Created img directory: " << dir_img);
+
+	return dir_data;
+}
+
+std::string
 rovi_system::make_custom_data_dir(const std::string& experiment_name, const std::string& dir)
 {
 	if (dir.empty() or dir[0] != '/')
 		throw std::invalid_argument("The provided directory '" + dir + "' cannot be empty and must begin with '/' in make_custom_data_dir().");
-	
+
 	auto dir_data = get_experiment_dir(experiment_name) + "/data" + dir;
 	if (std::filesystem::create_directories(dir_data))
 		ROS_INFO_STREAM("Created data directory: " << dir_data);
-		
+
 	return dir_data;
 }
