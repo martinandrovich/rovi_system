4fb3699022243dd97a0c4ec45cb719bf90712cdc
Martin <martinandrovich@gmail.com>
2021-12-01 14:55:45 +0100

    update API

--- rovi_vision/examples/demo_rovi_vision_RANSACRegistrationWithICP.cpp
+++ rovi_vision/examples/demo_rovi_vision_RANSACRegistrationWithICP.cpp
@@ -15,14 +15,10 @@ int main(int argc, char** argv)
 	auto cloud_obj = pcl::load_cloud("package://rovi_models/models/milk/milk.pcd"); // from ros_utils
 
 	// get point cloud from workcell
-	auto cloud_sene = gazebo::kinect.get_cloud(); // from ros_utils
+	auto cloud_sene = gazebo::kinect().get_cloud(); // from ros_utils
 
 	// estimate pose using global registration
 	auto pose = rovi_vision::RANSACRegistrationWithICP::est_pose(cloud_scene, cloud_obj);
-
-	// estimate pose using global registration with pre-set object
-	rovi_vision::RANSACRegistrationWithICP::set_obj(cloud_obj);
-	auto pose = rovi_vision::RANSACRegistrationWithICP::est_pose(cloud_scene);
 
 	return 0;
 }
