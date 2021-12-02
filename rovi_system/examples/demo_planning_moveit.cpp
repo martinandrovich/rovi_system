1d9f30d6f0641a5bfcff943129d75d6afc16c1bf
Martin <martinandrovich@gmail.com>
2021-12-02 14:41:43 +0100

    update planning_moveit demo

--- rovi_system/examples/demo_planning_moveit.cpp
+++ rovi_system/examples/demo_planning_moveit.cpp
@@ -20,7 +20,8 @@ int
 main(int argc, char **argv)
 {
 	// usage:
-	// roslaunch rovi_system workcell.launch objects:=false moveit:=true rviz:=true pose_robot:="-x 0.4 -y 0.55"
+	// roslaunch rovi_system workcell.launch objects:=false moveit:=true rviz:=true projector:=false pose_robot:="-x 0.4 -y 0.55"
+	// rosrun rovi_system demo_planning_moveit [obj_pos_x] [obj_pos_y]
 
 	// ------------------------------------------------------------------------------
 
@@ -33,6 +34,7 @@ main(int argc, char **argv)
 	// setup simulation + scene
 	ROS_INFO_STREAM("Setting up simulation...");
 	gazebo::set_simulation(true);
+	gazebo::delete_model("bottle1");
 	wsg50::release();
 	// gazebo::projector().set(false);
 	ros::Duration(1.0).sleep(); // settle
@@ -40,8 +42,12 @@ main(int argc, char **argv)
 	// go home, you're drunk
 	ur5::command_home();
 
+	// object position from args
+	auto pos_x = (argc >= 2) ? atof(argv[1]) : 0.15;
+	auto pos_y = (argc >= 3) ? atof(argv[2]) : 1.05;
+
 	// define pose of object in base frame with some offset (moveit will plan for b_T_tcp)
-	auto pose_obj = geometry_msgs::make_pose({ 0.15, 1.05, 0.75 });
+	auto pose_obj = geometry_msgs::make_pose({ pos_x, pos_y, 0.75 });
 	auto w_T_obj = Eigen::make_tf(pose_obj);
 	auto pose_obj_tcp = geometry_msgs::make_pose(ur5::w_T_b().inverse() * w_T_obj * Eigen::make_tf({ 0, 0, 0.1 }));
 
@@ -61,20 +67,23 @@ main(int argc, char **argv)
 	// define tolerances for planning at TCP
 	// this allows end-effector to grasp object at a "free-rotating" z-axis
 	auto tolerances = std::array{
-		std::vector(3, 0.001),        // pos {xyz} [m]
-		std::vector{0.01, 0.01, 3.14} // ori {rpy} [rad]
+		std::vector(3, 0.0001),        // pos {xyz} [m]
+		std::vector{0.001, 0.001, 3.14} // ori {rpy} [rad]
 	};
 
 	// configure planner (if needed)
-	// ur5::moveit::set_planner_config(Planner::EST, "property", 1.0);
+	// ur5::moveit::set_planner_config(Planner::SBL, "property", 1.0);
 
 	// make plan and trajectory
-	auto plan = ur5::moveit::plan(pose_obj_tcp, Planner::EST, "tcp", tolerances, 5.0, 100);
+	auto plan = ur5::moveit::plan(pose_obj_tcp, Planner::SBL, "tcp", tolerances);
 	auto traj = ur5::moveit::plan_to_jnt_traj(plan);
 
 	ENTER_TO_CONTINUE("execute trajectory");
-
 	ur5::command_traj(traj);
+
+	// settle and update planning scene
+	ros::Duration(1.0).sleep();
+	ur5::moveit::update_planning_scene_from_gazebo();
 
 	// ------------------------------------------------------------------------------
 
