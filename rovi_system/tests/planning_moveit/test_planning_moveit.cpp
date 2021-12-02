49d8abfaeef71f1cd7fac62d5f77f8dc94063d2f
Martin <martinandrovich@gmail.com>
2021-12-02 14:40:52 +0100

    update planning_moveit test

--- rovi_system/tests/planning_moveit/test_planning_moveit.cpp
+++ rovi_system/tests/planning_moveit/test_planning_moveit.cpp
@@ -24,7 +24,11 @@ int
 main(int argc, char **argv)
 {
 	// usage:
+	// roslaunch rovi_system test_planning_moveit pick_index:=2
+
+	// or manually:
 	// roslaunch rovi_system workcell.launch objects:=false moveit:=true rviz:=true pose_robot:="-x 0.4 -y 0.55"
+	// rosrun rovi_system test_planning_moveit [pick_index]
 
 	// ------------------------------------------------------------------------------
 
@@ -34,7 +38,7 @@ main(int argc, char **argv)
 
 	// parameters
 	auto NUM_ITER = 50u;
-	auto PICK_INDEX = (argc >= 2) ? atoi(argv[1]) : 2; // default index
+	auto PICK_INDEX = (argc >= 2) ? atoi(argv[1]) : 2; // default index (0, 1, or 2)
 	auto OBJ_NAME = std::string("bottle");
 
 	// ------------------------------------------------------------------------------
@@ -43,7 +47,6 @@ main(int argc, char **argv)
 	ROS_INFO_STREAM("Setting up simulation...");
 	gazebo::set_simulation(true);
 	wsg50::release();
-	// gazebo::projector().set(false);
 	ros::Duration(5.0).sleep(); // settle
 
 	// go home, you're drunk
@@ -52,6 +55,7 @@ main(int argc, char **argv)
 	// spawn obstacles
 	ROS_INFO_STREAM("Spawning obstacles and object...");
 	rovi_system::spawn_obstacles();
+	gazebo::delete_model(OBJ_NAME + "1");
 	gazebo::spawn_model(OBJ_NAME, OBJ_NAME + "1", PICK_LOCATIONS[PICK_INDEX]);
 
 	// init moveit
@@ -59,35 +63,43 @@ main(int argc, char **argv)
 
 	// load collision objects from Gazebo
 	ur5::moveit::update_planning_scene_from_gazebo();
-	ur5::moveit::start_scene_publisher(1); // Hz
+	ur5::moveit::start_scene_publisher(10); // Hz
+
+	ROS_WARN_STREAM("Starting with pick index: " << PICK_INDEX);
+	ros::Duration(2.0).sleep();
 
 	// ------------------------------------------------------------------------------
-	
+
 	// define pose of object in base frame with some offset; moveit will plan for b_T_tcp
 	auto w_T_obj = Eigen::make_tf(PICK_LOCATIONS[PICK_INDEX]);
 	auto pose_obj_tcp = geometry_msgs::make_pose(ur5::w_T_b().inverse() * w_T_obj * Eigen::make_tf({ 0, 0, 0.1 }));
 
 	// define tolerances for planning at TCP
 	// this allows end-effector to grasp object at a "free-rotating" z-axis
-	auto tolerances = std::array{ std::vector(3, 0.001), std::vector{0.01, 0.01, 3.14}}; // pos [m], ori [rad]
+	auto tolerances = std::array{ std::vector(3, 0.001), std::vector{0.001, 0.001, 3.14}}; // pos [m], ori [rad]
 
 	// ------------------------------------------------------------------------------
 
 	// make dirs for data (test_planning_moveit/data/XXXXXX/<planner>)
 	auto dir_data = rovi_system::make_timestamped_data_dirs("planning_moveit", {
-		ur5::moveit::PLANNERS[Planner::RRT],
-		ur5::moveit::PLANNERS[Planner::RRTConnect]
+		ur5::moveit::PLANNERS[Planner::SBL],
+		ur5::moveit::PLANNERS[Planner::EST]
 	});
 
 	// export meta-data
 	std::ofstream fs(dir_data + "/info.txt", std::ostream::out);
-	fs << "object: " << OBJ_NAME << "\npick_index: " << PICK_INDEX << "\nnum_iter: " << NUM_ITER << "\nw_T_obj:\n" << w_T_obj.matrix().format({Eigen::StreamPrecision, Eigen::DontAlignCols, ", ", "\n"});
+	fs << "object: " << OBJ_NAME << "\n"
+	   << "pick_index: " << PICK_INDEX << "\n"
+	   << "num_iter: " << NUM_ITER << "\n"
+	   << "tol_pos: " << tolerances[0] << "\n"
+	   << "tol_ori: " << tolerances[1] << "\n"
+	   << "w_T_obj:\n" << w_T_obj.matrix().format({Eigen::StreamPrecision, Eigen::DontAlignCols, ", ", "\n"});
 	fs.close();
 
 	// ------------------------------------------------------------------------------
 
 	// do experiments for each planner
-	for (auto planner : { Planner::RRT, Planner::RRTConnect })
+	for (auto planner : { Planner::SBL, Planner::EST })
 	{
 		using Traj = trajectory_msgs::JointTrajectory;
 		using Plan = rovi_planner::PlanningData<Traj>;
@@ -109,10 +121,9 @@ main(int argc, char **argv)
 
 			if (plan_moveit.error_code_.val == moveit_msgs::MoveItErrorCodes::SUCCESS)
 			{
-				// ur5::moveit::export_ctraj(*plan_moveit.trajectory_, dir_data_planner + "/traj_pre" + std::to_string(i) + ".csv"); // before trajectory simplification
-				plan.traj = ur5::moveit::plan_to_jnt_traj(plan_moveit);
+				plan.traj = ur5::moveit::plan_to_jnt_traj(plan_moveit); // simplifies trajectory
 				plan.traj_duration = plan_moveit.trajectory_->getDuration();
-				ur5::moveit::export_ctraj(*plan_moveit.trajectory_, dir_data_planner + "/traj" + std::to_string(i) + ".csv"); // after trajectory simplification
+				ur5::moveit::export_ctraj(*plan_moveit.trajectory_, dir_data_planner + "/traj" + std::to_string(i) + ".csv", "tcp"); // after trajectory simplification
 			}
 
 			results.push_back(plan);
@@ -122,10 +133,10 @@ main(int argc, char **argv)
 		rovi_planner::export_planning_data(results, dir_data_planner + "/plan.csv");
 	}
 
+	ROS_WARN_STREAM("Finished experiments!");
+
 	// ------------------------------------------------------------------------------
 
-	ROS_WARN_STREAM("Finished experiments!");
-	
 	// exit cleanly (or at least try to, lol)
 	ur5::moveit::terminate();
 
