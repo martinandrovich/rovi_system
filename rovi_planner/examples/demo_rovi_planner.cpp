558c6d6750879436483a6d4e779de98a8eded274
Martin <martinandrovich@gmail.com>
2021-12-04 15:20:38 +0100

    update demo

--- rovi_planner/examples/demo_rovi_planner.cpp
+++ rovi_planner/examples/demo_rovi_planner.cpp
@@ -3,28 +3,27 @@
 #include <tuple>
 
 #include <ros/ros.h>
-#include <rovi_planner/rovi_planner.h>
 #include <ros_utils/geometry_msgs.h>
+#include <rovi_planner/rovi_planner.h>
 
 int main(int argc, char** argv)
 {
-	using namespace geometry_msgs;
-
 	ros::init(argc, argv, "demo_rovi_planner");
 	ros::NodeHandle nh;
 
 	// define waypoints
 	std::vector<geometry_msgs::Pose> waypoints =
 	{
-		make_pose({ 0.0, 0.0, 0.0 }, { 0,      0, 0 }),
-		make_pose({ 0.0, 0.0, 1.0 }, { 0, M_PI/2, 0 }),
-		make_pose({ 2.0, 1.0, 0.0 }, { 0,      0, 0 }),
+		geometry_msgs::make_pose({ 0.0, 0.0, 0.0 }, { 0,      0, 0 }),
+		geometry_msgs::make_pose({ 1.0, 0.0, 0.0 }, { 0, M_PI/2, 0 }),
+		geometry_msgs::make_pose({ 2.0, 1.0, 0.0 }, { 0,      0, 0 }),
+		geometry_msgs::make_pose({ 1.0, 2.0, 0.0 }, { 0,      0, 0 }),
 	};
 
-	// parameters
-	auto [VEL_MAX, ACC_MAX, CORNER_RADIUS, EQUIV_RADIUS] = std::tuple{ 0.1, 0.1, 0.05, 0.001 };
+	// params
+	double VEL_MAX = 0.2, ACC_MAX = 0.05, CORNER_RADIUS = 0.2, EQUIV_RADIUS = 0.01;
 
-	// trajectories
+	// generate trajectories
 	auto traj_lin = rovi_planner::traj_lin(waypoints, VEL_MAX, ACC_MAX, EQUIV_RADIUS);
 	auto traj_par = rovi_planner::traj_par(waypoints, VEL_MAX, ACC_MAX, CORNER_RADIUS, EQUIV_RADIUS);
 
@@ -40,8 +39,10 @@ int main(int argc, char** argv)
 	for (auto [t, T] = std::tuple{ 0., vec_T.begin()}; t < dur; T++, t += dt )
 		std::cout << t << "/" << dur << " [sec]:\n\n" << T->matrix() << "\n\n";
 
-	// export trajectories to be plotted in MATLAB
-	// ...
+	// // export trajectories to be plotted in MATLAB
+	// rovi_planner::export_traj(traj_lin, "traj_lin.csv");
+	// rovi_planner::export_traj(traj_par, "traj_par.csv");
+	// rovi_planner::export_waypoints(waypoints, "waypoints.csv");
 
 	return 0;
 }
